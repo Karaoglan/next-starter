e6ee40958b9206114e37fd3769b966cb30edde2b
bk <burakkaraoglan55@gmail.com>
2023-06-19 14:26:12 +0300

    fix: add script tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,38 +13,34 @@ export default function Document() {
           crossOrigin='anonymous'
         />
 
-        <script
+        <Script
+          id='tag-manager-script'
+          type='text/javascript'
+          async
           dangerouslySetInnerHTML={{
-            __html:
-              `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
-              `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
-              `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
-              `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
-              `})(window,document,'script','dataLayer','GTM-MLZPWTZ');`,
+            __html: `
+              (function(w, d, s, l, i) {
+                    w[l] = w[l] || []
+                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+                    const f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s),
+                      dl = l != "dataLayer" ? "&l=" + l : ""
+                    j.async = true
+                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+                    f.parentNode.insertBefore(j, f)
+                  })(window,document,'script','dataLayer', 'GTM-MLZPWTZ')`,
           }}
         />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html:
-              `window.dataLayer = window.dataLayer || [];` +
-              `function gtag(){dataLayer.push(arguments);}` +
-              `gtag('js', new Date());` +
-              `gtag('config', 'G-9DCXWVL804');`,
-          }}
-        ></script>
       </Head>
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src='https://www.googletagmanager.com/ns.html?id=GTM-MLZPWTZ' height='0' width='0' style='display:none;visibility:hidden' />`,
-          }}
-        ></noscript>
-
-        <Script
-          async
-          src='https://www.googletagmanager.com/gtag/js?id=G-9DCXWVL804'
-        ></Script>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-MLZPWTZ'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
 
         <Main />
         <NextScript />
